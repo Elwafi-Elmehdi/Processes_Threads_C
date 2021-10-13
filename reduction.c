e48e2635aec11051e280c91479c9cf3941fc67ce
Mehdi <elwafi.courrier99@gmail.com>
2021-10-13 16:21:15 +0100

    Updated reduction.c

--- reduction.c
+++ reduction.c
@@ -1,5 +1,8 @@
+// REASON : calculate redution offer prise from init prise
+// AUTHOR : mehdi
+// CREATED DATE : 2021-10-13 16:20:09
+
 #include <stdio.h>
-// 
 int main(int argc, char const *argv[])
 {
     /* code */
--- template.sh
+++ template.sh
@@ -8,11 +8,11 @@ name=$1
 reason=$2
 
 cat <<-EOF > ./$name.c
-#include <stdio.h>
 // REASON : `echo $reason`
 // AUTHOR : `echo $USER`
 // CREATED DATE : $(date +"%F %T")
 
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */

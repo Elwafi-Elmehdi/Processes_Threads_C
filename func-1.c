79fd7e040b5bdf9ac15b0b5ab8e53ca0da1cabfe
Mehdi <elwafi.courrier99@gmail.com>
2021-10-13 16:55:25 +0100

    Added func-1.c

---
+++ func-1.c
@@ -0,0 +1,10 @@
+// REASON : calculate img of f(x)=(2x+3)(3x^2+2)
+// AUTHOR : mehdi
+// CREATED DATE : 2021-10-13 16:54:55
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

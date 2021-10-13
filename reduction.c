cfec132a6727417bc579daaf34635f64a66558ca
Mehdi <elwafi.courrier99@gmail.com>
2021-10-13 16:51:05 +0100

    Updated project structure

--- reduction.c
+++ reduction.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    double initPrise, offrePrise;
+    short percentage = 10;
+
+    printf("ENTRER THE INITIAL PRISE: \n");
+    scanf("%lf", &initPrise);
+
+    offrePrise = initPrise - (initPrise / percentage);
+    printf("THE OFFER PRISE IS: %lf \n", offrePrise);
+
     return 0;
 }

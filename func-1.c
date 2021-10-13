107fac29d54c055eece9871c11f0d65b0e37e0d1
Mehdi <elwafi.courrier99@gmail.com>
2021-10-13 16:59:39 +0100

    UPdated func-1.c

--- func-1.c
+++ func-1.c
@@ -3,8 +3,20 @@
 // CREATED DATE : 2021-10-13 16:54:55
 
 #include <stdio.h>
+#include <math.h>
+
+double f(double x)
+{
+    return (2 * x + 3) * (3 * pow(x, 2) + 2)
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    double x;
+
+    printf("ENTRE X:\n");
+    scanf("%lf", &x);
+    printf("F(%.2lf) = %lf", x, f(x));
+
     return 0;
 }

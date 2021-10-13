4fc740ff55d871bbdd92dd0f9fef427149af3bef
Mehdi <elwafi.courrier99@gmail.com>
2021-10-13 15:55:50 +0100

    Added ignore file and updated f2c.c

--- f2c.c
+++ f2c.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    // Declaration des variables
+    double c, f;
+    // Saisie de la var f
+    printf("ENTREZ LA TEMPERATURE EN F");
+    scanf("%lf", &f);
+    // Calcul de la temperature en degree C
+    c = 0.55556l * (f - 32);
+    printf("%3lf F = %3lf C \n", f, c);
     return 0;
 }

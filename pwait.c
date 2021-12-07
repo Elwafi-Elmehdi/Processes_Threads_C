b8d0ef394feb9cdb6f22b3e2b89ae33ab1c67be5
Mehdi <elwafi.courrier99@gmail.com>
2021-12-07 17:33:51 +0100

    Completed tp 1 exercice 3

--- pwait.c
+++ pwait.c
@@ -6,21 +6,46 @@
 
 int main(int argc, char *argv[])
 {
-    for (int i = 0; i < argc; i++)
+    if (argc != 2)
     {
-        pid_t pid = fork();
-        if (getppid() != 0)
+        exit(-1);
+    }
+    int status;
+    pid_t pid;
+    system("clear");
+
+    for (int i = 0; i < atoi(argv[1]); i++)
+    {
+        pid = fork();
+        if (pid == -1)
         {
-            printf("Je suis le fils : %d", pid);
+            // Pas de memoire pour creer des processus
+            exit(-1);
+        }
+        else if (pid != 0)
+        {
+            continue;
+        }
+        else
+        {
             sleep(2 * i);
+            printf("Je suis le fils : %d \net voila mon pere : %d \n\n", (long)getpid(), (long)getppid());
             exit(i);
         }
     }
-    // while (wait( == -1)
-    // {
-    //     int pid = wait();
-    //     printf("%d", pid);
-    // }
+
+    while (1)
+    {
+        int pidfils = wait(&status);
+        if (pidfils == -1)
+        {
+            break;
+        }
+        else
+        {
+            printf("Processus Termine: %d avec le code status : %d\n\n", pidfils, WEXITSTATUS(status));
+        }
+    }
 
     return 0;
 }

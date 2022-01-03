b6b7aa778e1bf68547e5274fe45b2c7939988906
Mehdi <elwafi.courrier99@gmail.com>
2022-01-03 11:09:19 +0100

    Finished serie 1 ex 8

--- serie-1-ex-8.c
+++ serie-1-ex-8.c
@@ -6,8 +6,28 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <pthread.h>
+void *genererTab()
+{
+    int tab[5];
+    for (int i = 0; i < 5; i++)
+    {
+        tab[i] = rand() / 99;
+    }
+    pthread_exit((void *)tab);
+}
 int main(int argc, char const *argv[])
 {
-    /* code */
+
+    void *status;
+    int *tableau;
+    pthread_t t;
+    pthread_create(&t, NULL, genererTab, NULL);
+    pthread_join(t, &status);
+    tableau = (int *)status;
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Tab[%d] = %d\n", i, tableau[i]);
+    }
     return 0;
 }

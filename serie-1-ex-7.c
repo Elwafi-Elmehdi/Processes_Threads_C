82857d352bb63785c33806f5604d6f74c6485cfe
Mehdi <elwafi.courrier99@gmail.com>
2022-01-03 10:07:38 +0100

    Added serie 1 ex 7

---
+++ serie-1-ex-7.c
@@ -0,0 +1,13 @@
+// REASON : thread qui affiche un tableau d'entiers
+// AUTHOR : mehdi
+// CREATED DATE : 2022-01-03 10:07:25
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

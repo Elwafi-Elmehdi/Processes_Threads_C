57ae06bb9512ea94fe50f9f8f66c7911beea3051
Mehdi <elwafi.courrier99@gmail.com>
2022-01-03 09:53:51 +0100

    Added ex 6 serie 1

---
+++ serie-1-ex-6.c
@@ -0,0 +1,30 @@
+// REASON : creation d'un thread qui dort
+// AUTHOR : mehdi
+// CREATED DATE : 2022-01-03 09:47:12
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *dormir(void *duree)
+{
+    int *d = (int *)duree;
+    sleep(*d);
+    free(duree);
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t thread;
+    if (argc = !2)
+    {
+        printf("Nombre d'arguments invalid");
+        exit(1);
+    }
+    pthread_create(&thread, NULL, dormir, (void *)&argv[1]);
+    pthread_join(thread, 0);
+    return 0;
+}

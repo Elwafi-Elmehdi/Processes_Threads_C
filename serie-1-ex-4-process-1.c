7924ab9c1338005ae6d13c1d44d4f6008ba6472b
Mehdi <elwafi.courrier99@gmail.com>
2022-01-03 10:30:57 +0100

    Added serie 1 ex 4

---
+++ serie-1-ex-4-process-1.c
@@ -0,0 +1,9 @@
+// REASON : processus qui modifie la valeur d'un variable global
+// AUTHOR : mehdi
+// CREATED DATE : 2022-01-03 10:07:25
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <pthread.h>
--- serie-1-ex-7.c
+++ serie-1-ex-7.c
@@ -6,8 +6,25 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <pthread.h>
+void *afficher_tableau(void *tableau)
+{
+    int *tab = (int *)tableau;
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Bonjour, je suis le thread secondaire, et voila ce que j'ai recu du thread principale");
+        printf("Tab[%d] = %d\n", i, tab[i]);
+    }
+}
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int tab[5];
+    pthread_t t;
+    for (int i = 0; i < 5; i++)
+    {
+        tab[i] = i * 2;
+    }
+    pthread_create(&t, NULL, afficher_tableau, (void *)&tab);
+    pthread_join(t, 0);
     return 0;
 }

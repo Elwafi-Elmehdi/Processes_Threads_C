56845dcca894f18e3cfe6cf09321b1eb6caed1a7
Mehdi <elwafi.courrier99@gmail.com>
2022-01-03 09:24:09 +0100

    Added ex 3 serie 2

--- pthreads/serie-2-ex-2.c
+++ pthreads/serie-2-ex-2.c
@@ -8,30 +8,31 @@ struct parms
     int counter;
 };
 
-void *print(void *structa);
+void *afficher(void *structa)
+{
+    struct parms *s = (struct parms *)structa;
+    for (int i = 0; i < (*s).counter; i++)
+    {
+        printf("%c\n", (*s).character);
+    }
+    free(structa);
+    return NULL;
+}
 
 int main(int argc, char const *argv[])
 {
     pthread_t thread1;
     pthread_t thread2;
     struct parms A, B;
+
     A.character = 'x';
     A.counter = 3000;
     B.character = 'o';
     B.counter = 2000;
-    pthread_create(&thread1, NULL, print, (void *)&A);
-    pthread_create(&thread2, NULL, print, (void *)&B);
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    pthread_create(&thread1, NULL, afficher, (void *)&A);
+    pthread_create(&thread2, NULL, afficher, (void *)&B);
+    // le process pere qui a cree les deux thread n'attend meme pas leur executions d'ou vient interre d'utilisation de fonction pthread_join.
+    // pthread_join(thread1, 0);
+    // pthread_join(thread2, 0);
     return 0;
-}
-
-void *print(void *structa)
-{
-    struct parms *s = (struct parms *)structa;
-
-    for (int i = 0; i < s->counter; i++)
-    {
-        printf("%s\n", s->character);
-    }
 }

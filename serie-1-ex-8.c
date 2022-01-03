e22804834742dad4bda713f09586bb3ed6ddd040
Mehdi <elwafi.courrier99@gmail.com>
2022-01-03 10:54:54 +0100

    Adde serie 1 ex 8

---
+++ serie-1-ex-8.c
@@ -0,0 +1,13 @@
+// REASON : thread allout un tab des entiers aliatoirement
+// AUTHOR : mehdi
+// CREATED DATE : 2022-01-03 10:54:41
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

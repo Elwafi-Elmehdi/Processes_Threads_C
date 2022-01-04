d0bbd2c8a379d74c6a3a4950d4a04817536c24de
Mehdi <elwafi.courrier99@gmail.com>
2022-01-04 14:05:11 +0100

    Updated serie 2 exercice 7

--- pthreads/serie-2-ex-7.c
+++ pthreads/serie-2-ex-7.c
@@ -14,13 +14,20 @@ int main(int argc, char const *argv[])
 {
     int N;
     int pid_fils;
+    void *CHILD_STACK = (void *)malloc(1024 * 1024);
     if (argc != 2)
     {
         exit(1);
     }
     N = atoi(argv[1]);
-    pid_fils = clone(table_multiplication, malloc(1024 * 1024), CLONE_NEWUTS | SIGCHLD, (void *)&N);
-
+    pid_fils = clone(table_multiplication, CHILD_STACK, CLONE_CHILD_CLEARTID, (void *)&N);
+    if (pid_fils == -1)
+    {
+        exit(2);
+    }
+    int *tab = (int *)CHILD_STACK;
+    for (int i = 1; i < 11; i++)
+        printf("%d x %d = %d\n", N, i, tab[i]);
     return 0;
 }
 

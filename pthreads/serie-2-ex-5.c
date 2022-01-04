dd72b3157dc89476910c5faf26f58b5b320c8e1d
Mehdi <elwafi.courrier99@gmail.com>
2022-01-04 10:45:55 +0100

    Added serie 2 ex 5

---
+++ pthreads/serie-2-ex-5.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+struct job
+{
+    struct job *next;
+    char character;
+    int count;
+};
+struct job *job_queue;
+
+// La definition de la fonction enqueue_job
+void enqueue_job(char chart, int count)
+{
+    struct job *nouveau_job = (struct job *)malloc(sizeof(struct job));
+    nouveau_job->count = count;
+    nouveau_job->character = chart;
+    nouveau_job->next = job_queue;
+    job_queue = nouveau_job;
+}
+
+// La definition de la fonction process_job
+void process_job(struct job *joba)
+{
+    for (int i = 0; i < (*joba).count; i++)
+    {
+        printf("%c\n", (*joba).character);
+    }
+}
+
+void *thread_function(void *arg)
+{
+    while (job_queue != NULL)
+    {
+        struct job *next_job = job_queue; /* Récupère la tâche suivante.*/
+        job_queue = job_queue->next;      /* Supprime cette tâche de la liste.*/
+        process_job(next_job);            /* Traite la tâche.*/
+        // sleep(2);                         // Le programme se plante car sleep() met le thread executant en attente pendant les secondes specifiees dans sleep.
+        free(next_job); /* Libération des ressources.*/
+    }
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t t1, t2;
+    enqueue_job('M', 5);
+    enqueue_job('S', 6);
+    enqueue_job('O', 9);
+    pthread_create(&t1, NULL, thread_function, NULL);
+    pthread_create(&t2, NULL, thread_function, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    return 0;
+}

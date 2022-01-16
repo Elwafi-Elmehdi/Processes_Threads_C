9439b93968326bd34a554e7180607d6a07fc24d7
Mehdi <elwafi.courrier99@gmail.com>
2022-01-16 15:56:21 +0100

    Added serie-1-ex-3

---
+++ serie-1-ex-3.c
@@ -0,0 +1,13 @@
+// REASON : Communication entre deux processus en utilisant pipe()
+// AUTHOR : mehdi
+// CREATED DATE : 2022-01-16 15:55:56
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

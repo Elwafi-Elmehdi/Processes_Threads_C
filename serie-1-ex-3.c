173b55e0a5e249dbf98ccfa5cba9f443624ea817
Mehdi <elwafi.courrier99@gmail.com>
2022-01-16 17:54:41 +0100

    Updated exercice 3

--- serie-1-ex-3.c
+++ serie-1-ex-3.c
@@ -5,9 +5,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int fd[2];
+    char *message;
+    int status = pipe(fd);
+    if (status == -1)
+    {
+        perror("Error PIPE()");
+        exit(-1);
+    }
+
+    switch (fork())
+    {
+    case -1:
+        perror("Error FORK()");
+        exit(-2);
+        break;
+    case 0:
+        close(fd[0]);
+        char *message = "Bonjour, je suis processus fils";
+        write(fd[1], &message, sizeof(message));
+        close(fd[1]);
+        break;
+    default:
+        close(fd[1]);
+        read(fd[0], &message, sizeof(message));
+        close(fd[0]);
+        wait(NULL);
+        printf("\nBonjour je suis le processus pere, et j'ai recu \"%s\"", message);
+        break;
+    }
     return 0;
 }
